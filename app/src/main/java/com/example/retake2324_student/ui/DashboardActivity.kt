12719124a75cc5887540c0191ed475d3f9afb7bd
NotForTrade <147601558+NotForTrade@users.noreply.github.com>
2024-05-24 17:17:49 +0200

    Clean-up

--- app/src/main/java/com/example/retake2324_student/ui/DashboardActivity.kt
+++ app/src/main/java/com/example/retake2324_student/ui/DashboardActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 
